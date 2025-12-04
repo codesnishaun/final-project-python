cfa5e2a24afd000fab5d6ac24e9915364cc975b2
codesnishaun <shaunaaronnaga.basc@gmail.com>
2025-12-04 22:08:00 +0800

    added vincent part and prince

--- final_project.py
+++ final_project.py
@@ -1,9 +1,32 @@
 def main(): #johnray
-    pass
+    while True:
+        print("\n=== Library Management System ===")
+        print("1. Add a Book")
+        print("2. Edit Existing Book")
+        print("3. Delete a Book")
+        print("4. List All Books")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-5): ").strip()
+
+        if choice == "1":
+            add_book()
+        elif choice == "2":
+            edit_existing_book()
+        elif choice == "3":
+            delete_book()
+        elif choice == "4":
+            list_all_books()
+        elif choice == "5":
+            print("Exiting program... Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a number from 1 to 5.")
 
 books = {} #empty dict
 
 def add_book():
+
     print("\n--- Add a Book ---")
     isbn = input("Enter ISBN (13 digits): ").strip()
 
@@ -13,6 +36,7 @@ def add_book():
 
     if isbn in books:
         print("A book with this ISBN already exist.")
+        return
 
     title = input("Enter Title: ").strip().title()
     author = input("Enter Author: ").strip().title()
@@ -33,9 +57,24 @@ def edit_existing_book(): #liezel
     pass
 
 def delete_book(): #vincent
-    pass
+    print("\n--- Delete a Book ---")
+    isbn = input("Enter ISBN of the book to delete: ").strip()
+
+    if isbn not in books:
+        print("Book not found.")
+        return
+
+    confirm = input(f"Are you sure you want to delete '{books[isbn]['title']}'? (y/n): ").lower()
+
+    if confirm == "y":
+        del books[isbn]
+        print("Book deleted successfully.")
+    else:
+        print("Delete cancelled.")
 
 def list_all_books(): #chelzy
-    pass
+    for book in books.values():
+        print(book)
+    # pass
 
 main()
